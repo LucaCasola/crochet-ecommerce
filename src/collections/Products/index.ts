067b3d8e4555e3ab7df0c57b9e9760fa87a79474
Luca Casola <lcasola@myseneca.ca>
2024-12-30 17:55:23 -0500

    create Products collection
    -create Products collection
    -include it in payload.config.ts

---
+++ src/collections/Products/index.ts
@@ -0,0 +1,32 @@
+import type { CollectionConfig } from 'payload'
+
+export const Products: CollectionConfig = {
+  slug: 'products',
+  fields: [
+    {
+      name: 'title',
+      label: 'Title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'price',
+      label: 'Price',
+      type: 'number',
+      required: true,
+    },
+    {
+      name: 'description',
+      label: 'Description',
+      type: 'textarea',
+      required: true,
+    },
+    {
+      name: 'image',
+      label: 'Image',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+  ],
+}
